0f203cbcd8ed4bfb48ac11646c456da6412e2af2
Anoop Chandran <anoop.chandran@reconnects.com>
2019-07-17 03:06:10 +0800

    Updates

---
+++ precache-manifest.2762ec4c53601303dddb067c96114a5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edb70f061979402b2560",
+    "url": "/users-app-react/static/js/runtime~main.91d67177.js"
+  },
+  {
+    "revision": "5625d2453dec89988d42",
+    "url": "/users-app-react/static/js/main.f7d09f11.chunk.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/users-app-react/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "5625d2453dec89988d42",
+    "url": "/users-app-react/static/css/main.a512edf2.chunk.css"
+  },
+  {
+    "revision": "48c5e62204e06176b62a95dd6d6524db",
+    "url": "/users-app-react/index.html"
+  }
+];
